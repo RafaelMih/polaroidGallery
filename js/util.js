1edb5cb3175cdfe7cfd8cca7ff157c968e349ff8
Rafael Mih <rafaelwsantos@gmail.com>
2019-05-19 10:15:47 -0300

    audio

--- js/util.js
+++ js/util.js
@@ -29,3 +29,61 @@ function shuffle(array) {
         }
     return array;
 }
+
+function volumeHide(hidden, src) {
+    if (hidden) {
+        $("#volume").hide();
+        $("#audio").hide();
+        audioStop();
+    } else {
+        $("#volume").show();
+        audioPlay(src);
+    }
+}
+
+function audioPlay(src) {
+    if (src) {
+        $("#audio").attr('src', src);
+        $("#audio").trigger('play');
+    } else {
+        audioStop();
+    }
+}
+
+function audioStop() {
+    $("#audio").trigger('pause');
+    $("#audio").currentTime = 0.0;
+}
+
+function loadGallery() {
+    var html = "";
+    var galleryPhotos = [];
+
+    for (var a = [], i = 0; i < countGalleryPhotos; ++i)
+        galleryPhotos[i] = i;
+
+    galleryPhotos.forEach(index => {
+        var name = getMotherName(index);
+        html += `<div onclick="showSlide(${index})" class="polaroid ui-draggable col-xs-1 col-md-3 col-lg-2">
+                    <img src="./images/maes/${index}/f.jpg" alt="${name}" />
+                    <p>${name}</p>
+                 </div>`;
+    });
+
+    $("#galeria").html(html);
+}
+
+function loadSlideShow(index) {
+    var name = getMotherName(index);
+    var countPhotos = getCountPhotos(index);
+    var html = "";
+    for (let i = 0; i < countPhotos; i++) {
+        html += `<li>
+                    <a href="./images/maes/${index}/${i}.jpg">
+                        <img src="./images/maes/${index}/${i}.jpg" title="${name}" data-valign="middle"/>
+                    </a>
+                </li>`;
+    }
+
+    $("#listPhotos").html(html);
+}
